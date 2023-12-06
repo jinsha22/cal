d4801ee687374fc39e5799d0deedac5d311e0dfb
unknown <jinshajinuzz22@gmail.com>
2023-12-06 23:33:08 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,13 @@
+
+function displayData(e){
+    result.value=result.value+e
+}
+function out(){
+    result.value=eval(result.value)
+}
+function Allclear(){
+    result.value=""
+}
+function Delete(){
+     result.value=result.value.slice(0,-1)
+}
